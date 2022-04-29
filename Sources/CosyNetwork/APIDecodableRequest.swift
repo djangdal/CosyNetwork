363f67c4329063d8826011fe51374f7a7250131f
David Jangdal <djangdal@gmail.com>
2022-04-29 12:26:34 +0200

    Added better error handling and removed authenticated dispatcher

--- Sources/CosyNetwork/APIDecodableRequest.swift
+++ Sources/CosyNetwork/APIDecodableRequest.swift
@@ -1,6 +1,5 @@
 //
 //  Request.swift
-//  Trooper
 //
 //  Created by David Jangdal on 2021-03-25.
 //
@@ -16,23 +15,32 @@ public enum HTTPMethod: String {
     case delete
 }
 
+enum APIRequestError: Error {
+    case unableToGetUrlComponents
+    case unableToCreateUrl
+}
+
 public protocol APIRequest {
+    associatedtype ErrorBodyType: Decodable & Error
+
     // Required
     var baseURLPath: String { get }
     var path: String { get }
     var method: HTTPMethod { get }
+    var failingStatusCodes: [HTTPStatusCode] { get }
 
     //Optional
     var queryParameters: [String: String]? { get }
-    var requestHeaders: [String: String]? { get }
+    var requestHeaders: [String: String]? { get set }
     var cachingPolicy: URLRequest.CachePolicy { get }
     var encoder: JSONEncoder { get }
 
-    var urlRequest: URLRequest? { get }
+    var urlRequest: URLRequest { get throws }
 }
 
 public protocol APIDecodableRequest: APIRequest {
     associatedtype ResponseBodyType: Decodable
+    var successStatusCodes: [HTTPStatusCode] { get }
 }
 
 public protocol APIEncodableRequest: APIRequest {
@@ -54,41 +62,52 @@ public extension APIRequest {
 }
 
 fileprivate extension APIRequest {
-    var baseRequest: URLRequest? {
-        guard var urlComponents = URLComponents(string: baseURLPath + path) else { return nil }
+    var baseRequest: URLRequest {
+        get throws {
+            guard var urlComponents = URLComponents(string: baseURLPath + path) else {
+                throw APIRequestError.unableToGetUrlComponents
+            }
 
-        if urlComponents.queryItems == nil || urlComponents.queryItems?.isEmpty == true {
-            urlComponents.queryItems = queryParameters?.map { URLQueryItem(name: $0, value: $1) }
-        } else if let queryParams = queryParameters, var queryItems = urlComponents.queryItems {
-            queryItems.append(contentsOf: queryParams.map { URLQueryItem(name: $0, value: $1) })
-            urlComponents.queryItems = queryItems
+            if urlComponents.queryItems == nil || urlComponents.queryItems?.isEmpty == true {
+                urlComponents.queryItems = queryParameters?.map { URLQueryItem(name: $0, value: $1) }
+            } else if let queryParams = queryParameters, var queryItems = urlComponents.queryItems {
+                queryItems.append(contentsOf: queryParams.map { URLQueryItem(name: $0, value: $1) })
+                urlComponents.queryItems = queryItems
+            }
+
+            guard let url = urlComponents.url else {
+                throw APIRequestError.unableToCreateUrl
+            }
+
+            var urlRequest = URLRequest(url: url)
+            urlRequest.httpMethod = method.rawValue.uppercased()
+            urlRequest.cachePolicy = cachingPolicy
+            requestHeaders?.forEach { (key: String, value: String) in
+                urlRequest.addValue(value, forHTTPHeaderField: key)
+            }
+            return urlRequest
         }
-
-        guard let url = urlComponents.url else { return nil }
-
-        var urlRequest = URLRequest(url: url)
-        urlRequest.httpMethod = method.rawValue.uppercased()
-        urlRequest.cachePolicy = cachingPolicy
-        requestHeaders?.forEach { (key: String, value: String) in
-            urlRequest.addValue(value, forHTTPHeaderField: key)
-        }
-        return urlRequest
     }
 }
 
 public extension APIRequest {
-    var urlRequest: URLRequest? {
-        baseRequest
+    var urlRequest: URLRequest {
+        get throws {
+            try baseRequest
+        }
     }
 }
 
 public extension APIEncodableRequest {
-    var urlRequest: URLRequest? {
-        guard var urlRequest = baseRequest, let data = try? encoder.encode(body) else { return nil }
-        if urlRequest.allHTTPHeaderFields?["Content-Type"] == nil {
-            urlRequest.allHTTPHeaderFields?["Content-Type"] = "application/json"
+    var urlRequest: URLRequest {
+        get throws {
+            var urlRequest = try baseRequest
+            if urlRequest.allHTTPHeaderFields?["Content-Type"] == nil {
+                urlRequest.allHTTPHeaderFields?["Content-Type"] = "application/json"
+            }
+            let data = try encoder.encode(body)
+            urlRequest.httpBody = data
+            return urlRequest
         }
-        urlRequest.httpBody = data
-        return urlRequest
     }
 }
--- Sources/CosyNetwork/APIDispatcher.swift
+++ Sources/CosyNetwork/APIDispatcher.swift
@@ -1,6 +1,5 @@
 //
 //  Dispatcher.swift
-//  Trooper
 //
 //  Created by David Jangdal on 2021-03-25.
 //
@@ -9,20 +8,17 @@ import Foundation
 import Combine
 
 public enum APIError: Error {
-    case urlRequestUnavailable
-}
-
-public struct ResponseError: Error, Decodable {
-    public let error: String
-    public let errorCode: String
+    case invdalidHttpResponse
+    case couldNotCreateHTTPStatusCode
+    case statusCodeNotHandled
 }
 
 public protocol APIDispatcherProtocol {
-    func dispatch<Request: APIRequest>(_ request: Request) async throws
-    func dispatch<Request: APIDecodableRequest>(_ request: Request) async throws -> Request.ResponseBodyType 
+    @discardableResult func dispatch<Request: APIRequest>(_ request: Request) async throws -> (Data, HTTPURLResponse, HTTPStatusCode)
+    func dispatch<Request: APIDecodableRequest>(_ request: Request) async throws -> (Request.ResponseBodyType, HTTPURLResponse, HTTPStatusCode)
 }
 
-public final class APIDispatcher: APIDispatcherProtocol {
+open class APIDispatcher: APIDispatcherProtocol {
     private let urlSession: URLSession
     private let decoder: JSONDecoder
 
@@ -32,73 +28,36 @@ public final class APIDispatcher: APIDispatcherProtocol {
     }
 
     @discardableResult
-    private func execute<Request: APIRequest>(_ request: Request) async throws -> Data {
-        guard let urlRequest = request.urlRequest else {
-            throw APIError.urlRequestUnavailable
-        }
+    private func execute<Request: APIRequest>(_ request: Request) async throws -> (Data, HTTPURLResponse, HTTPStatusCode) {
+        let urlRequest = try request.urlRequest
         let response = try await urlSession.data(for: urlRequest)
 
         guard let httpResponse = response.1 as? HTTPURLResponse else {
-            throw ResponseError(error: "Could not get HTTPURLResponse from URLRespose", errorCode: "none")
+            throw APIError.invdalidHttpResponse
         }
 
-        if httpResponse.statusCode == 400 {
-            throw try decoder.decode(ResponseError.self, from: response.0)
+        guard let statusCode = HTTPStatusCode(rawValue: httpResponse.statusCode) else {
+            throw APIError.couldNotCreateHTTPStatusCode
         }
-        return response.0
-    }
 
-    public func dispatch<Request: APIRequest>(_ request: Request) async throws {
-        try await execute(request)
-    }
-
-    public func dispatch<Request: APIDecodableRequest>(_ request: Request) async throws -> Request.ResponseBodyType {
-        let data = try await execute(request)
-        return try decoder.decode(Request.ResponseBodyType.self, from: data)
-    }
-}
-
-public class APIAuthenticatedDispatcher: APIDispatcherProtocol {
-    private let urlSession: URLSession
-    private var token: String
-    private let authHeaderName: String
-    private let decoder: JSONDecoder
-    
-    public init(urlSession: URLSession = .shared, token: String, authHeaderName: String, decoder: JSONDecoder = JSONDecoder()) {
-        self.urlSession = urlSession
-        self.token = token
-        self.authHeaderName = authHeaderName
-        self.decoder = decoder
+        if request.failingStatusCodes.contains(statusCode) {
+            throw try decoder.decode(Request.ErrorBodyType.self, from: response.0)
+        }
+        return (response.0, httpResponse, statusCode)
     }
 
     @discardableResult
-    public func execute<Request: APIRequest>(_ request: Request) async throws -> Data {
-        guard var urlRequest = request.urlRequest else {
-            throw APIError.urlRequestUnavailable
-        }
-        urlRequest.addValue(token, forHTTPHeaderField: authHeaderName)
-        let response = try await urlSession.data(for: urlRequest)
-
-        guard let httpResponse = response.1 as? HTTPURLResponse else {
-            throw ResponseError(error: "Could not get HTTPURLResponse from URLRespose", errorCode: "none")
-        }
-
-        if let token = httpResponse.value(forHTTPHeaderField: self.authHeaderName) {
-            self.token = token
-        }
-
-        if httpResponse.statusCode == 400 {
-            throw try decoder.decode(ResponseError.self, from: response.0)
-        }
-        return response.0
-    }
-
-    public func dispatch<Request: APIRequest>(_ request: Request) async throws {
+    open func dispatch<Request: APIRequest>(_ request: Request) async throws -> (Data, HTTPURLResponse, HTTPStatusCode) {
         try await execute(request)
     }
 
-    public func dispatch<Request: APIDecodableRequest>(_ request: Request) async throws -> Request.ResponseBodyType {
-        let response = try await execute(request)
-        return try decoder.decode(Request.ResponseBodyType.self, from: response)
+    open func dispatch<Request: APIDecodableRequest>(_ request: Request) async throws -> (Request.ResponseBodyType, HTTPURLResponse, HTTPStatusCode) {
+        let (data, urlResponse, statusCode) = try await execute(request)
+
+        if request.successStatusCodes.contains(statusCode) {
+            return (try decoder.decode(Request.ResponseBodyType.self, from: data), urlResponse, statusCode)
+        }
+
+        throw APIError.statusCodeNotHandled
     }
 }
